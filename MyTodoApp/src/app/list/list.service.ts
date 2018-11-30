482ce6ab72b1854bf2672041297701f1c1144b6d
Pietro Sacco <pietro.sacco@bbv.ch>
2018-11-30 11:15:00 +0100

    Uses ionic/storage to save data.

--- MyTodoApp/src/app/list/list.service.ts
+++ MyTodoApp/src/app/list/list.service.ts
@@ -1,30 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
+
+const kDataBaseKey = 'asd98s7d09f8s9f';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListService {
 
-    private data: TodoItem[];
-
-    constructor() {
-        this.data = [
-            { title: 'Buy bread', note: 'Lorem ipsum dolor', completed: false },
-            { title: 'Call auntie', note: 'Dolores et ea rebum.', completed: false },
-            { title: 'Watch "The Godfather"', note: 'Justo duo dolores et ea rebum', completed: false },
-            { title: 'Have fun!', note: 'Consetetur sadipscing elitr', completed: false }
-        ];
-    }
+    constructor(private storage: Storage) { }
 
     getItems(): Promise<TodoItem[]> {
-        // Return a copy
-        return Promise.resolve(this.data.slice());
+        return this.storage.get(kDataBaseKey).then((val) => {
+            return val || [];
+          });
     }
 
     saveItems(newItems: TodoItem[]) {
-        // Make a copy
-        this.data = newItems.slice();
-        console.log('saved');
+        this.storage.set(kDataBaseKey, newItems);
     }
 }
 
